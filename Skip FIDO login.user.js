93406de50b7f7fea269aa6c8c356696cb8c01820
NoobChester <60832458+NoobChester@users.noreply.github.com>
2020-09-03 08:13:48 +0800

    FUCK YOU error NO ERRORS ALLOWED

--- Skip FIDO login.user.js
+++ Skip FIDO login.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Skip FIDO login
-// @version      0.2
+// @version      0.2.1
 // @description  Skip FIDO login page
 // @author       NoobChester
 // @match        https://adfs.asas.qld.edu.au/adfs/ls/*
 // @require      http://code.jquery.com/jquery-3.5.1.slim.min.js
 // ==/UserScript==
-
+/* globals jQuery, $, waitForKeyElements */
 $(function() {
     setTimeout(function(){
         document.getElementById('submitButton').dispatchEvent(new MouseEvent("click"));
