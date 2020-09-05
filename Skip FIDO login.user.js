29aed6301012f841c4943ff386cee3e31648acfa
Chester Au <60832458+NoobChester@users.noreply.github.com>
2020-09-05 10:38:58 +0800

    1.3 Improve load time and stability by spamming the command

--- Skip FIDO login.user.js
+++ Skip FIDO login.user.js
@@ -1,29 +1,16 @@
 // ==UserScript==
 // @name         Skip FIDO login
-// @version      1.2
+// @version      1.3
 // @description  Skip FIDO login page
 // @author       NoobChester
 // @match        https://adfs.asas.qld.edu.au/adfs/ls/*
-// @require      http://code.jquery.com/jquery-3.5.1.slim.min.js
 // ==/UserScript==
-/* globals jQuery, $, waitForKeyElements */
-
-document.getElementById('submitButton').dispatchEvent(new MouseEvent("click"));
-
-$(function() {
-    setTimeout(function(){
-        document.getElementById('submitButton').dispatchEvent(new MouseEvent("click"));
-    }, 31);
-});
-
-$(function() {
-    setTimeout(function(){
-        document.getElementById('submitButton').dispatchEvent(new MouseEvent("click"));
-    }, 314);
-});
-
-$(function() {
-    setTimeout(function(){
-        document.getElementById('submitButton').dispatchEvent(new MouseEvent("click"));
-    }, 3141);
-});
+var counter = 0;
+var i = setInterval(function(){
+    // do your thing
+    document.getElementById('submitButton').dispatchEvent(new MouseEvent("click"));
+    counter++;
+    if(counter === 9) {
+        clearInterval(i);
+    }
+}, 9*i);
